f5b5ffd645bdbe485f2ef20ee58bbb9011e08d96
giorgip62 <giorgiop62@gmail.com>
2022-12-05 17:22:24 +0100

    commit iniziale

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <script src=“https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js” integrity=“sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==” crossorigin=“anonymous” referrerpolicy=“no-referrer”></script>
+    <script src=“https://unpkg.com/vue@3/dist/vue.global.js”></script>
+  <title>PHP Dischi JSON</title>
+</head>
+<body>
+  <header>
+    <img src="https://www.geekslab.it/wp-content/uploads/2019/03/logo-spotify.png" alt="logo">
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="card" style="width: 18rem; background-color:#12202f;">
+          <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <p class="card-text"></p>
+            </div>
+        </div>
+      
+
+      </div>
+
+    </div>
+
+  </div>
+
+
+
+
+
+  
+</body>
+</html>
